e37ba587db0a982b8471f703650ea3488b1358bc
zhangheng <onethousandpiece@126.com>
2019-09-23 18:27:28 +0800

    feat: code 23

--- Python/Code23.py
+++ Python/Code23.py
@@ -1,13 +1,11 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 from Python.Tool.ListNode import ListNode
+
 class Solution:
     def mergeKLists(self, lists: [ListNode]) -> ListNode:
-        listNodes = ListNode
+        listNodes = []
+        for list in lists:
+            if type(list) == ListNode:
+                listNodes.append(list)
         rootNode = None
         currentNode = None
         while len(listNodes) > 0:
@@ -29,4 +27,30 @@ class Solution:
                 currentNode = minNode
         return rootNode
 
-Solution().mergeKLists([[1], [1,3,4], [2,6]])
+# # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+#
+# class Solution:
+#     def mergeKLists(self, lists: [ListNode]) -> ListNode:
+#         import heapq
+#         dummy = ListNode(0)
+#         p = dummy
+#         head = []
+#         for i in range(len(lists)):
+#             if lists[i] :
+#                 heapq.heappush(head, (lists[i].val, i))
+#                 lists[i] = lists[i].next
+#         while head:
+#             val, idx = heapq.heappop(head)
+#             p.next = ListNode(val)
+#             p = p.next
+#             if lists[idx]:
+#                 heapq.heappush(head, (lists[idx].val, idx))
+#                 lists[idx] = lists[idx].next
+#         return dummy.next
+# Solution().mergeKLists([[1], [1,3,4], [2,6]])
+
+Solution().mergeKLists([ListNode([1]), ListNode([1,3,4]), ListNode([2,6])])
