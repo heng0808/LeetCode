7b356fc1eeae4f6be3befb58e050128d6444d859
zhangheng <onethousandpiece@126.com>
2019-09-23 11:44:33 +0800

    feat: update

--- Python/Code22.py
+++ Python/Code22.py
@@ -1,40 +1,43 @@
-class Solution:
-    def generateParenthesis(self, n: int) -> [str]:
-        parenthesis = []
-        def append(current:str, sign:str, leftcount:int, rightcount:int):
-            current = current + sign
-            if len(current) < 2 * n:
-                append(current, '(', leftcount, rightcount)
-                append(current, ')', leftcount, rightcount)
-            else:
-                parenthesis.append(current)
-        if n == 0:
-            return parenthesis
-        else:
-            append('', '', 0, 0)
-            return parenthesis
-        # parenthesis = []
-        # def append(current:str, sign:str, leftcount:int, rightcount:int):
-        #     if sign == '(':
-        #         leftcount = leftcount + 1
-        #     else:
-        #         rightcount = rightcount + 1
-        #     current = current + sign
-        #     if rightcount <= leftcount:
-        #         if leftcount < n and rightcount < n:
-        #             append(current, '(', leftcount, rightcount)
-        #             append(current, ')', leftcount, rightcount)
-        #         elif leftcount < n:
-        #             append(current, '(', leftcount, rightcount)
-        #         elif rightcount < n:
-        #             append(current, ')', leftcount, rightcount)
-        #         else:
-        #             parenthesis.append(current)
-        # if n == 0:
-        #     return parenthesis
-        # else:
-        #     append('', '(', 0, 0)
-        #     return parenthesis
+# class Solution:
+#     def generateParenthesis(self, n: int) -> [str]:
+#         parenthesis = []
+#         def append(current:str, sign:str, leftcount:int, rightcount:int):
+#             if sign == '(':
+#                 leftcount = leftcount + 1
+#             else:
+#                 rightcount = rightcount + 1
+#             current = current + sign
+#             if rightcount <= leftcount:
+#                 if leftcount < n and rightcount < n:
+#                     append(current, '(', leftcount, rightcount)
+#                     append(current, ')', leftcount, rightcount)
+#                 elif leftcount < n:
+#                     append(current, '(', leftcount, rightcount)
+#                 elif rightcount < n:
+#                     append(current, ')', leftcount, rightcount)
+#                 else:
+#                     parenthesis.append(current)
+#         if n == 0:
+#             return parenthesis
+#         else:
+#             append('', '(', 0, 0)
+#             return parenthesis
 
-print(Solution().generateParenthesis(3))
-print(len(Solution().generateParenthesis(3)))
+class Solution(object):
+    def generateParenthesis(self, N):
+        ans = []
+        def backtrack(S = '', left = 0, right = 0):
+            if len(S) == 2 * N:
+                ans.append(S)
+                return
+            if left < N:
+                backtrack(S+'(', left+1, right)
+            if right < left:
+                backtrack(S+')', left, right+1)
+
+        backtrack()
+        return ans
+
+result = Solution().generateParenthesis(3)
+print(len(result))
+print(result)
