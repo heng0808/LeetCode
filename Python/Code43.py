6bd7d66f9790f395edd32a1ce11c8afb0e3206c4
zhangheng <111@11.com>
2020-01-14 01:43:13 +0800

    feat: empty 43 45 55

---
+++ Python/Code43.py
@@ -0,0 +1,8 @@
+from typing import List
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        ans = ""
+        for charcter1 in num1[::-1]:
+            for charcter2 in num2[::-1]:
+                pass
+print(Solution().multiply("123", "456"))
