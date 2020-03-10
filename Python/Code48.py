8e487beb00b741c99640778319ab362af08b1540
zhangheng <onethousandpiece@126.com>
2020-03-11 00:54:49 +0800

    feat: Code48 Solution2

--- Python/Code48.py
+++ Python/Code48.py
@@ -1,10 +1,7 @@
 from typing import List
 
 class Solution:
-    def rotate(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
+    def rotate1(self, matrix: List[List[int]]) -> None:
         level = 0
         length = len(matrix) - 1
         count = len(matrix)
@@ -21,9 +18,33 @@ class Solution:
             count = count - 2
             level = level + 1
 
-# matrix = [[1,2,3],[4,5,6],[7,8,9]]
-matrix = [[ 5, 1, 9,11],[ 2, 4, 8,10],[13, 3, 6, 7],[15,14,12,16]]
+    def rotate2(self, matrix: List[List[int]]) -> None:
+        for i in range(0, len(matrix)):
+            for j in range(i, len(matrix)):
+                temp = matrix[i][j]
+                matrix[i][j] = matrix[j][i]
+                matrix[j][i] = temp
+        for i in range(0, len(matrix)):
+            matrix[i].reverse()
 
-Solution().rotate(matrix)
+# 先转置，再求逆矩
+# [[ 1, 2, 3, 4],
+#  [ 5, 6, 7, 8],
+#  [ 9,10,11,12],
+#  [13,14,15,16]]
+# ----------
+# [[1,4,7],
+#  [2,5,8],
+#  [3,6,9]]
+# ----------
+# [[7,4,1],
+#  [8,5,2],
+#  [9,6,3]]
+
+
+# matrix = [[1,2,3],[4,5,6],[7,8,9]]
+matrix = [[ 1, 2, 3, 4],[ 5, 6, 7, 8],[ 9,10,11,12],[13,14,15,16]]
+
+Solution().rotate2(matrix)
 
 print(matrix)
