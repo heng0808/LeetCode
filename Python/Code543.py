da7436bcdac5d40fe66e12cb84cc9f0091a86ce3
zhangheng <onethousandpiece@126.com>
2020-03-10 19:58:54 +0800

    feat: MySelf Code543

---
+++ Python/Code543.py
@@ -0,0 +1,19 @@
+from Tool.TreeNode import TreeNode
+import sys
+
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        ans = 0
+        def depthNode(node:TreeNode) -> int:
+            if node == None:
+                return 0
+            nonlocal ans
+            l = depthNode(node.left)
+            r = depthNode(node.right)
+            ans = max(ans, l + r + 1)
+            return max(l, r) + 1
+        depthNode(root)
+        return ans - 1 if ans >1 else 0
+
+tree = TreeNode[int].node([1,2,3,4,5])
+print(Solution().diameterOfBinaryTree(tree))
