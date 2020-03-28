0c4e02ae3452a61c09bac6bd737ff779f39e0e2e
zhangheng <onethousandpiece@126.com>
2020-03-28 22:34:16 +0800

    feat: finish code71

---
+++ Python/Code71.py
@@ -0,0 +1,25 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        splits = path.split('/')
+        split_paths = []    
+        for folder in splits:
+            if len(folder) == 0:
+                continue
+            elif folder == '.':
+                continue
+            elif folder == '..':
+                if len(split_paths) > 0:
+                    split_paths.pop()
+            else:
+                split_paths.append(folder)
+        
+        path = ''
+        for folder in split_paths:
+            path = path + '/' + folder
+        return path if len(path) > 0 else '/'
+
+print(Solution().simplifyPath('/home//foo/'))
+print(Solution().simplifyPath('/home/'))
+print(Solution().simplifyPath('/../'))
+print(Solution().simplifyPath('/a/../../b/../c//.//'))
+print(Solution().simplifyPath('/a/./b/../../c/'))
