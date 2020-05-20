a40571243bd5a58ca0c37f6ae68b3278297194e9
zhangheng <onethousandpiece@126.com>
2020-05-20 14:31:15 +0800

    feature: 1

--- 1.两数之和.py
+++ 1.两数之和.py
@@ -1,18 +1,19 @@
+#
+# @lc app=leetcode.cn id=1 lang=python3
+#
+# [1] 两数之和
+#
+from typing import List
+# @lc code=start
 class Solution:
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for index1 in range(0, len(nums) - 1):
-            num_left = target - nums[index1]
-            nums_left = nums[index1 + 1:]
-            try:
-                return [index1, nums_left.index(num_left) + index1 + 1]
-            except BaseException as error:
-                continue
+    def twoSum(self, nums, target) -> List[int]:
+        num_map = {}
+        for index in range(0, len(nums)):
+            num = nums[index]
+            res = target - num
+            if res in num_map:
+                return [index, num_map[res]]
+            num_map[num] = index
         return []
-if __name__ == '__main__':
-    reulst = Solution().twoSum([0,4,3,0], 0)
-    pass
+# @lc code=end
+
