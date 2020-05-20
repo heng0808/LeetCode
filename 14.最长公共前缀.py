1312045b2b7eecfba754e25aea912409848d3ea5
zhangheng <onethousandpiece@126.com>
2020-05-20 18:57:23 +0800

    feature: 14

--- 14.最长公共前缀.py
+++ 14.最长公共前缀.py
@@ -1,27 +1,33 @@
-class Solution:
-    def longestCommonPrefix(self, strs: [str]) -> str:
-        max_str = ""
-        index = 0
-        run = True
-        while run:
-            try:
-                char = strs[0][index]
-            except:
-                run = False
-            else:
-                if len(strs) > 1:
-                    for str in strs[1:]:
-                        try:
-                            char_other = str[index]
-                            if char != char_other:
-                                run = False
-                                break
-                        except:
-                            run = False
-                            break
-                if run:
-                    max_str = max_str + char
-                    index = index + 1
-        return max_str
+#
+# @lc app=leetcode.cn id=14 lang=python3
+#
+# [14] 最长公共前缀
+#
 
-Solution().longestCommonPrefix(["abc", "abcd", "abcde"])
+# @lc code=start
+class Solution:
+    def longestCommonPrefix(self, strs: []):
+        ans = ''
+        index = 0
+        while True:
+            character = None
+            for _str in strs:
+                if index < len(_str):
+                    if character == None:
+                        character = _str[index]
+                    else:
+                        if character != _str[index]:
+                            character = None
+                            break
+                else:
+                    character = None
+                    break
+            if character == None:
+                break
+            else:
+                ans = ans + character
+            index = index + 1
+        return ans
+# @lc code=end
+print(Solution().longestCommonPrefix(["aa","a"]))
+
