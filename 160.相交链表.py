a0f82d1b63d8ba3d4a01d5d0e531ace929edb7e3
zhangheng1 <zhangheng1@vipkid.com.cn>
2020-05-21 13:16:07 +0800

    feat: 160

---
+++ 160.相交链表.py
@@ -0,0 +1,47 @@
+#
+# @lc app=leetcode.cn id=160 lang=python3
+#
+# [160] 相交链表
+#
+from Tool.ListNode import ListNode
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        lengthA = 0
+        nodeA = headA
+        while nodeA != None:
+            nodeA = nodeA.next
+            lengthA = lengthA + 1
+
+        lengthB = 0
+        nodeB = headB
+        while nodeB != None:
+            nodeB = nodeB.next
+            lengthB = lengthB + 1
+
+        nodeA = headA
+        nodeB = headB
+        if lengthA > lengthB:
+            while lengthA > lengthB:
+                nodeA = nodeA.next
+                lengthA = lengthA - 1
+        elif lengthB > lengthA:
+            while lengthB > lengthA:
+                nodeB = nodeB.next
+                lengthB = lengthB - 1
+
+        while nodeA != None and nodeB != None:
+            if nodeA == nodeB:
+                return nodeA
+            else:
+                nodeA = nodeA.next
+                nodeB = nodeB.next
+        return None
+# @lc code=end
+
--- 21.合并两个有序链表.py
+++ 21.合并两个有序链表.py
@@ -1,54 +1,55 @@
+#
+# @lc app=leetcode.cn id=21 lang=python3
+#
+# [21] 合并两个有序链表
+#
+from Tool.ListNode import ListNode
+# @lc code=start
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-from Tool.ListNode import ListNode
-
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        if l1 == None and l2 == None:
-            return None
-        elif l1 == None:
-            return l2
-        elif l2 == None:
-            return l1
-        else:
-            l1Node = l1
-            l2Node = l2
-            rootNode = None
-            node = None
-            if l1Node.val < l2Node.val:
-                rootNode = l1Node
-                node = l1Node
-                l1Node = l1Node.next
+        headNode = None
+        preNode = None
+        while l1 != None or l2 != None:
+            if l1 == None:
+                if headNode == None:
+                    headNode = l2
+                if preNode != None:
+                    preNode.next = l2
+                preNode = l2
+                l2 = l2.next
+                preNode.next = None
+            elif l2 == None:
+                if headNode == None:
+                    headNode = l1
+                if preNode != None:
+                    preNode.next = l1
+                preNode = l1
+                l1 = l1.next
+                preNode.next = None
             else:
-                rootNode = l2Node
-                node = l2Node
-                l2Node = l2Node.next
-
-            while l1Node != None or l2Node != None:
-                if l1Node == None:
-                    node.next = l2Node
-                    node = node.next
-                    l2Node = l2Node.next
-                elif l2Node == None:
-                    node.next = l1Node
-                    node = node.next
-                    l1Node = l1Node.next
+                if l1.val < l2.val:
+                    if headNode == None:
+                        headNode = l1
+                    if preNode != None:
+                        preNode.next = l1
+                    preNode = l1
+                    l1 = l1.next
+                    preNode.next = None
                 else:
-                    if l1Node.val < l2Node.val:
-                        node.next = l1Node
-                        node = node.next
-                        l1Node = l1Node.next
-                    else:
-                        node.next = l2Node
-                        node = node.next
-                        l2Node = l2Node.next
-            return rootNode
+                    if headNode == None:
+                        headNode = l2
+                    if preNode != None:
+                        preNode.next = l2
+                    preNode = l2
+                    l2 = l2.next
+                    preNode.next = None
+        return headNode
 
-node1 = ListNode.node("124")
-node2 = ListNode.node("134")
-node = Solution().mergeTwoLists(node1, node2)
-pass
+# @lc code=end
+print(Solution().mergeTwoLists(ListNode.node([1,2,4]), ListNode.node([1,3,4])))
