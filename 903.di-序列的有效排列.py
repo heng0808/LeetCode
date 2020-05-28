e344bd89821f3d5e2939ac4151f669335444b221
zhangheng <onethousandpiece@126.com>
2020-05-29 01:03:16 +0800

    feat: not finish 903

---
+++ 903.di-序列的有效排列.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=903 lang=python3
+#
+# [903] DI 序列的有效排列
+#
+
+# @lc code=start
+class Solution:
+    def numPermsDISequence(self, S: str) -> int:
+        
+# @lc code=end
+
