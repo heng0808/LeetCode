0eb44b218e68d10fabf6dd24ab0ab898da5e916b
zhangheng <onethousandpiece@126.com>
2020-05-29 00:59:50 +0800

    feat: rewrite 71

--- 71.简化路径.py
+++ 71.简化路径.py
@@ -1,3 +1,10 @@
+#
+# @lc app=leetcode.cn id=71 lang=python3
+#
+# [71] 简化路径
+#
+
+# @lc code=start
 class Solution:
     def simplifyPath(self, path: str) -> str:
         splits = path.split('/')
@@ -17,9 +24,11 @@ class Solution:
         for folder in split_paths:
             path = path + '/' + folder
         return path if len(path) > 0 else '/'
-
+        
+# @lc code=end
 print(Solution().simplifyPath('/home//foo/'))
 print(Solution().simplifyPath('/home/'))
 print(Solution().simplifyPath('/../'))
 print(Solution().simplifyPath('/a/../../b/../c//.//'))
 print(Solution().simplifyPath('/a/./b/../../c/'))
+
