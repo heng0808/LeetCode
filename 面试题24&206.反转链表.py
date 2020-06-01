9eddf12c33c89b0ad322cb1891c71faa55fec07a
zhangheng <onethousandpiece@126.com>
2020-06-01 16:45:33 +0800

    feat: 面试题24

---
+++ 面试题24&206.反转链表.py
@@ -0,0 +1,18 @@
+from Tool.ListNode import ListNode
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        pre = None
+        node = head
+        while node != None:
+            nodeNext = node.next
+            if pre == None:
+                pre = node
+                node = nodeNext
+                pre.next = None
+            else:
+                node.next = pre
+                pre = node
+                node = nodeNext
+        return pre
+
+print(Solution().reverseList(ListNode.node([1,2,3,4,5])))
