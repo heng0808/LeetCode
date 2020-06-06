9688f3252ff3a4e2988b03c7538eb32b0492b094
zhangheng <onethousandpiece@126.com>
2020-06-07 02:58:32 +0800

    feat: 面试题22

---
+++ 面试题22.链表中倒数第k个节点.py
@@ -0,0 +1,17 @@
+from Tool.Python.ListNode import ListNode
+
+class Solution:
+    def getKthFromEnd(self, head: ListNode, k: int) -> ListNode:
+        leftNode, rightNode = head, head
+        while k > 1:
+            if rightNode.next == None:
+                return None
+            else:
+                k -= 1
+                rightNode = rightNode.next
+        while rightNode.next:
+            leftNode = leftNode.next
+            rightNode = rightNode.next
+        return leftNode
+
+print(Solution().getKthFromEnd(ListNode([1]), 1))
