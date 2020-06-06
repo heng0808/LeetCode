7df706bdec347a45031c6830c359112f9aadaa8d
zhangheng <onethousandpiece@126.com>
2020-06-07 00:41:43 +0800

    feat: review 94（中序）

---
+++ 94.二叉树的中序遍历.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode.cn id=94 lang=python3
+#
+# [94] 二叉树的中序遍历
+#
+from typing import List
+from Tool.TreeNode import TreeNode
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        # 左中右
+        # 默认当左，有左就扔，没左就弹，弹了看右
+        ans = []
+        stack = []
+        node = root
+        while len(stack) > 0 or node != None:
+            if node != None:
+                stack.append(node)
+                node = node.left
+            else:
+                node = stack.pop()
+                ans.append(node.val)
+                node = node.right
+        return ans
+        
+# @lc code=end
+print(Solution().inorderTraversal(TreeNode.node([1,None,2,None,None,3])))
