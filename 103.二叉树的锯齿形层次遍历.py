1cd31ebc7055e811188e6d0b025a05688e5ec574
zhangheng1 <zhangheng1@vipkid.com.cn>
2020-06-15 17:44:26 +0800

    feat: 103、124

---
+++ 103.二叉树的锯齿形层次遍历.py
@@ -0,0 +1,41 @@
+#
+# @lc app=leetcode.cn id=103 lang=python3
+#
+# [103] 二叉树的锯齿形层次遍历
+#
+from typing import List
+from Tool.Python.TreeNode import TreeNode
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+        nodes, level = [root] if root else [], 0
+        ans = []
+        while len(nodes) > 0:
+            _nodes = []
+            ans.append([])
+            if level % 2 == 0:
+                for node in nodes:
+                    ans[-1].append(node.val)
+                
+            else:
+                for node in reversed(nodes):
+                    ans[-1].append(node.val)
+            for node in nodes:
+                if node.left:
+                    _nodes.append(node.left)
+                if node.right:
+                    _nodes.append(node.right)
+            nodes = _nodes
+            level += 1
+        return ans
+# @lc code=end
+tree = TreeNode([3,9,20,None,None,15,7])
+print(Solution().zigzagLevelOrder(tree))
+
---
+++ 124.二叉树中的最大路径和.py
@@ -0,0 +1,46 @@
+#
+# @lc app=leetcode.cn id=124 lang=python3
+#
+# [124] 二叉树中的最大路径和
+#
+from Tool.Python.TreeNode import TreeNode
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def maxPathSum(self, root: TreeNode) -> int:
+        ans = root.val
+        def maxSubPathSum(node: TreeNode) -> int:
+            nonlocal ans
+            if node.left and node.right:
+                max_left = maxSubPathSum(node.left)
+                max_right = maxSubPathSum(node.right)
+                ans = max(max_left, max_right, max_left + max_right + node.val, ans)
+                return max(node.val, node.val + max(max_left, max_right))
+            elif node.left:
+                max_left = maxSubPathSum(node.left)
+                ans = max(max_left, max_left + node.val, ans)
+                return max(node.val, node.val + max_left)
+            elif node.right:
+                max_right = maxSubPathSum(node.right)
+                ans = max(max_right, max_right + node.val, ans)
+                return max(node.val, node.val + max_right)
+            else:
+                ans = max(node.val, ans)
+                return node.val
+        res = maxSubPathSum(root)
+        ans = max(ans, res)
+        return ans
+        
+# @lc code=end
+# tree = TreeNode([-1,5,None,4,None,None,None,2,-4])
+# tree = TreeNode([-10,9,20,None,None,15,7])
+# tree = TreeNode([1,2,3])
+tree = TreeNode([-1,5,None,4,None,None,2,-4])
+print(Solution().maxPathSum(tree))
+
