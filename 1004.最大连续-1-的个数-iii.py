3043d2e0c31f21091a18b58fee8150f6185d2a8a
zhangheng09 <zhangheng09@kuaishou.com>
2021-02-22 15:00:47 +0800

    feat: 1004

--- 1004.最大连续-1-的个数-iii.py
+++ 1004.最大连续-1-的个数-iii.py
@@ -6,43 +6,58 @@
 from typing import List
 # @lc code=start
 class Solution:
+    # 自己实现非滑动窗口
     def longestOnes(self, A: List[int], K: int) -> int:
-        def _longestOnes(_A: List[int]) -> int:
-            length = 0
-            result = 0
-            k_queue = []
-            for i in range(0, len(_A)):
-                num = _A[i]
-                if num == 0:
-                    if K > 0:
-                        if len(k_queue) == K:
-                            left = k_queue.pop(0)
-                            _A[left] = 0
-                            if left > 0 and _A[left - 1] == 1:
-                                length = len(k_queue)
-                            else:
-                                length -= 1
-                        _A[i] = 1
-                        k_queue.append(i)
-                        length += 1
-                    else:
-                        length = 0
-                elif num == 1:
+        length = 0
+        result = 0
+        k_queue = []
+        for i in range(0, len(A)):
+            num = A[i]
+            if num == 0:
+                if K > 0:
+                    if len(k_queue) == K:
+                        left = k_queue.pop(0)
+                        A[left] = 0
+                        if left > 0 and A[left - 1] == 1:
+                            length = i - (left + 1)
+                        else:
+                            length -= 1
+                    A[i] = 1
+                    k_queue.append(i)
                     length += 1
-                if length > result:
-                    result = length
-            return result
-        l_result = _longestOnes(A)
-        r_result = _longestOnes(list(reversed(A)))
-        return max(l_result, r_result)
+                else:
+                    length = 0
+            elif num == 1:
+                length += 1
+            if length > result:
+                result = length
+        return result
+    # 滑动窗口，边界算不明白了，放弃。。。
+    # def longestOnes(self, A: List[int], K: int) -> int:
+    #     left = -1
+    #     result = 0
+    #     k_count = 0
+    #     for i in range(0, len(A)):
+    #         if A[i] == 0:
+    #             k_count += 1
+    #         if k_count > K:
+    #             for j in range(left + 1, i):
+    #                 if A[j] == 0:
+    #                     left = j
+    #                     k_count -= 1
+    #         if result < i - left:
+    #             result = i - left
+    #     return result
 # @lc code=end
 # [1,1,1,0,0,0,1,1,1,1,0]
 # [1,1,1,1,0,0,1,1,1,1,0]
 # [1,1,1,1,1,0,1,1,1,1,0]
 # [1,1,1,0,1,1,1,1,1,1,0]
-# print(Solution().longestOnes([1,1,1,0,0,0,1,1,1,1,0], 2))
+print(Solution().longestOnes([1,1,1,0,0,0,1,1,1,1,0], 2))
 print(Solution().longestOnes([0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], 3))
-# print(Solution().longestOnes([0,0,1,1,1,0,0], 0))
-# print(Solution().longestOnes([1,1,1,0,0,0,1,1,1,1], 0))
-# print(Solution().longestOnes([1,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,1,1,1,1,0,1,0,1,1,1,1,1,1,0,1,0,1,0,0,1,1,0,1,1], 8))
-
+print(Solution().longestOnes([0,0,1,1,1,0,0], 0))
+print(Solution().longestOnes([1,1,1,0,0,0,1,1,1,1], 0))
+print(Solution().longestOnes([1,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,1,1,1,1,0,1,0,1,1,1,1,1,1,0,1,0,1,0,0,1,1,0,1,1], 8))
+          
+# [1,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,1,1,1,1,0,1,0,1,1,1,1,1,1,0,1,0,1,0,0,1,1,0,1,1]
+# [1,0,0,0,1,1,0,0,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
