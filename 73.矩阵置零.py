78a33fe33c9ce104163b6408dcfae55a55d87494
zhangheng09 <zhangheng09@kuaishou.com>
2021-03-21 11:12:08 +0800

    daily: code73 solution2

--- 73.矩阵置零.py
+++ 73.矩阵置零.py
@@ -12,18 +12,52 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rows = set()
-        cols = set()
-        for i in range(0, len(matrix)):
-            for j in range(0, len(matrix[0])):
-                if matrix[i][j] == 0:
-                    rows.add(i)
-                    cols.add(j)
+        # Solution1 O(m+n)
+        # rows = set()
+        # cols = set()
+        # for i in range(0, len(matrix)):
+        #     for j in range(0, len(matrix[0])):
+        #         if matrix[i][j] == 0:
+        #             rows.add(i)
+        #             cols.add(j)
 
-        for i in range(0, len(matrix)):
-            for j in range(0, len(matrix[0])):
-                if (i in rows or j in cols) and matrix[i][j] != 0:
+        # for i in range(0, len(matrix)):
+        #     for j in range(0, len(matrix[0])):
+        #         if (i in rows or j in cols) and matrix[i][j] != 0:
+        #             matrix[i][j] = 0
+
+        # Solution2 O(1)
+        row = len(matrix)
+        col= len(matrix[0])
+        row0_zero = False # 记录第一行是否有0
+        col0_zero = False # 记录第一列是否有0
+        for i in range(0, col):
+            if matrix[0][i] == 0:
+                row0_zero = True
+
+        for i in range(0, row):
+            if matrix[i][0] == 0:
+                col0_zero = True
+
+        for i in range(1, row):
+            for j in range(1, col):
+                if matrix[i][j] == 0:
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
+
+        for i in range(1, row):
+            for j in range(1, col):
+                if matrix[0][j] == 0 or matrix[i][0] == 0:
                     matrix[i][j] = 0
+
+        if row0_zero:
+            for i in range(0, col):
+                matrix[0][i] = 0
+        if col0_zero:
+            for i in range(0, row):
+                matrix[i][0] = 0
+        
+
 # @lc code=end
 
 
